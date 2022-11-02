e987d6946f754a0bd95fe8a61d997382aefa5baf
DEVpjy5658 <pjy5658@gmail.com>
2022-11-01 23:48:23 -0600

    Built a Pizza API

--- packages/server/src/application/providers/index.ts
+++ packages/server/src/application/providers/index.ts
@@ -1,9 +1,12 @@
 import { setupDb } from '../database';
 
 import { ToppingProvider } from './toppings/topping.provider';
+import { PizzaProvider } from './pizza/pizza.provider';
 
 const db = setupDb();
 
 const toppingProvider = new ToppingProvider(db.collection('toppings'));
+const pizzaProvider = new PizzaProvider(db.collection('pizzas'));
 
 export { toppingProvider };
+export { pizzaProvider };
