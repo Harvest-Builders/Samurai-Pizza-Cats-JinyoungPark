72fbbbb16ca8eddb7ccd8bddde5b7e6227bd0923
DEVpjy5658 <pjy5658@gmail.com>
2022-11-28 01:32:36 -0700

    6.1 Make the Pizza List Cards

--- packages/client/src/components/pizza/PizzaItem.tsx
+++ packages/client/src/components/pizza/PizzaItem.tsx
@@ -2,33 +2,42 @@ import { Pizza, Topping } from '../../types';
 import CardItem from '../common/CardItem';
 import { ListItem } from '@material-ui/core';
 import toDollars from '../../lib/format-dollars';
+import { makeStyles } from '@material-ui/styles';
+import { createStyles, Grid, Theme } from '@material-ui/core';
 
+const useStyles = makeStyles(({ typography, spacing }: Theme) =>
+  createStyles({
+    image: {
+      height: '65%',
+      width: '100%',
+    },
+  })
+);
 export interface PizzaItemProps {
   pizza?: Pizza;
   handleOpen: (pizza?: Pizza) => void;
 }
 const PizzaItem: React.FC<PizzaItemProps> = ({ pizza, handleOpen, ...props }) => {
-  const toppingList = pizza?.toppings.map((topping: Topping) => (
-    <li key={topping.id} data-testid={`topping-item-${topping?.id}`}>
-      {topping.name}
-    </li>
-  ));
+  const classes = useStyles();
 
   return (
-    <ListItem data-testid={`pizza-select-${pizza?.id}`} {...props} onClick={(): void => handleOpen(pizza)}>
+    <Grid
+      item
+      xs={4}
+      md={4}
+      data-testid={`pizza-select-${pizza?.id}`}
+      {...props}
+      onClick={(): void => handleOpen(pizza)}
+    >
       <CardItem>
-        <p data-testid={`pizza-name-${pizza?.id}`}>Pizza : {pizza?.name ?? 'Add Pizza'}</p>
-        <p data-testid={`pizza-description-${pizza?.description}`}>
-          Description : {pizza?.description ? pizza.description : ''}
-        </p>
+        <img className={classes.image} data-testid={`pizza-image-${pizza?.imgSrc}`} src={pizza?.imgSrc}></img>
+        <h1 data-testid={`pizza-name-${pizza?.id}`}>{pizza?.name ?? 'Add Pizza'}</h1>
+        <p data-testid={`pizza-description-${pizza?.id}`}>{pizza?.description ? pizza.description : ''}</p>
         <p data-testid={`pizza-priceCents-${pizza?.priceCents}`}>
-          Price : {pizza?.priceCents ? toDollars(pizza.priceCents) : ''}{' '}
+          {pizza?.priceCents ? toDollars(pizza.priceCents) : ''}{' '}
         </p>
-        <p>Toppings</p>
-        <ul>{toppingList}</ul>
-        <img data-testid={`pizza-image-${pizza?.imgSrc}`} src={pizza?.imgSrc}></img>
       </CardItem>
-    </ListItem>
+    </Grid>
   );
 };
 
