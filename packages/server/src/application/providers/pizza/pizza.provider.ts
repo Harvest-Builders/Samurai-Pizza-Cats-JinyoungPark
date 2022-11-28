f6d5d7b2d5277048a8418b3548a967301fb81d29
DEVpjy5658 <pjy5658@gmail.com>
2022-11-28 16:23:32 -0700

    Completed 6.2 Add Pagination to the Pizza Resolver

--- packages/server/src/application/providers/pizza/pizza.provider.ts
+++ packages/server/src/application/providers/pizza/pizza.provider.ts
@@ -1,35 +1,47 @@
 import { ObjectId, Collection } from 'mongodb';
 import { PizzaDocument, toPizzaObject } from '../../../entities/pizza';
-import { CreatePizzaInput, Pizza, UpdatePizzaInput } from './pizza.provider.types';
+import { CreatePizzaInput, Pizza, UpdatePizzaInput, PizzaResponse } from './pizza.provider.types';
 import validateStringInputs from '../../../lib/string-validator';
 import { ToppingProvider } from '../toppings/topping.provider';
+import { CursorProvider } from './cursor.provider';
+import { QueryInput } from '../../schema/types/schema';
 
 class PizzaProvider {
-  constructor(private collection: Collection<PizzaDocument>, private toppingProvider: ToppingProvider) {}
+  constructor(
+    private collection: Collection<PizzaDocument>,
+    private toppingProvider: ToppingProvider,
+    private cursorProvider: CursorProvider
+  ) {}
 
-  public async getPizzas(): Promise<Pizza[]> {
-    const pizzas = await this.collection.find().sort({ name: 1 }).toArray();
-    return pizzas.map(toPizzaObject);
+  public async getPizzas(input?: QueryInput): Promise<PizzaResponse> {
+    if (!input) return await this.cursorProvider.getCursorResult({ limit: 5, cursor: 'initial' });
+    const pizzaResponse = await this.cursorProvider.getCursorResult(input);
+    return pizzaResponse;
+  }
+  public async validateToppings(toppingsIds: string[]): Promise<void> {
+    const toppingIds_ObejctId: ObjectId[] = toppingsIds.map((id) => new ObjectId(id));
+    const numberOfToppings = (await this.toppingProvider.getToppingsByIds(toppingIds_ObejctId))?.length;
+    if (toppingsIds.length != numberOfToppings) {
+      throw new Error('Invalid toppings are included');
+    }
   }
 
   public async createPizza(input: CreatePizzaInput): Promise<Pizza> {
-    const { name, description, imgSrc, toppingIds } = input;
+    const stringInputs = [input.name, input.description, input.imgSrc];
 
-    if (name) validateStringInputs(name);
-    if (description) validateStringInputs(description);
-    if (imgSrc) validateStringInputs(imgSrc);
-    if (toppingIds) validateStringInputs(toppingIds);
+    // MongoDb request ObjectId type for it's id fields
+    const toppingIds_ObjectId = input.toppingIds.map((topping) => new ObjectId(topping));
+    validateStringInputs(stringInputs); // validate inputs
+    await this.validateToppings(input.toppingIds); // validate Toppings
 
     const data = await this.collection.findOneAndUpdate(
       { _id: new ObjectId() },
       {
         $set: {
-          ...(toppingIds && { toppingIds: toppingIds.map((toppingId) => new ObjectId(toppingId)) }),
-          ...(name && { name: name }),
-          ...(description && { description: description }),
-          ...(imgSrc && { imgSrc: imgSrc }),
-          updateAt: new Date().toISOString(),
-          createAt: new Date().toISOString(),
+          ...input,
+          toppingIds: toppingIds_ObjectId,
+          updatedAt: new Date().toISOString(),
+          createdAt: new Date().toISOString(),
         },
       },
       { upsert: true, returnDocument: 'after' }
@@ -38,7 +50,6 @@ class PizzaProvider {
     if (!data.value) {
       throw new Error(`Could not create the ${input.name} pizza`);
     }
-
     const pizza = data.value;
 
     return toPizzaObject(pizza);
@@ -62,27 +73,28 @@ class PizzaProvider {
 
   public async updatePizza(input: UpdatePizzaInput): Promise<Pizza> {
     const { id, name, description, imgSrc, toppingIds } = input;
-
-    if (name) validateStringInputs(name);
-
+    const toppingIds_ObjectId = toppingIds?.map((toppingId) => new ObjectId(toppingId));
+    const stringInputs = [name, description, imgSrc];
+    validateStringInputs(stringInputs);
+    if (toppingIds) {
+      await this.validateToppings(toppingIds);
+    }
     const data = await this.collection.findOneAndUpdate(
       { _id: new ObjectId(id) },
       {
         $set: {
-          ...(toppingIds && { toppingIds: toppingIds.map((toppingId) => new ObjectId(toppingId)) }),
           ...(name && { name: name }),
           ...(description && { description: description }),
           ...(imgSrc && { imgSrc: imgSrc }),
+          ...(toppingIds && { toppingIds: toppingIds_ObjectId }),
         },
       },
       { returnDocument: 'after' }
     );
-
     if (!data.value) {
-      throw new Error(`Could not update the pizza`);
+      throw new Error('could not update the pizza');
     }
     const pizza = data.value;
-
     return toPizzaObject(pizza);
   }
 }
