4e3c9078fa888c4b4bdd44701473a2896acba22b
DEVpjy5658 <pjy5658@gmail.com>
2022-11-28 16:36:16 -0700

    Edited the test in server

--- packages/server/test/helpers/pizza.helper.ts
+++ packages/server/test/helpers/pizza.helper.ts
@@ -1,15 +1,14 @@
 import { ObjectId } from 'bson';
 import { Pizza } from '../../src/application/schema/types/schema';
 import { PizzaDocument } from '../../src/entities/pizza';
-
+import { PizzaResponse } from '../../src/application/providers/pizza/pizza.provider.types';
 const createMockPizza = (data?: Partial<Pizza>): Pizza => {
   return {
     __typename: 'Pizza',
     id: new ObjectId().toHexString(),
     name: 'Test Pizza',
-    description: 'Test pizza description',
-    imgSrc:
-      'https://images.unsplash.com/photo-1627626775846-122b778965ae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80',
+    description: 'test Pizza description',
+    imgSrc: 'testImg',
     toppings: [
       {
         __typename: 'Topping',
@@ -28,17 +27,32 @@ const createMockPizza = (data?: Partial<Pizza>): Pizza => {
     ...data,
   };
 };
-
+const createMockPizzaResponse = (data?: Partial<PizzaDocument>): PizzaResponse => {
+  return {
+    results: [
+      {
+        id: new ObjectId().toString(),
+        name: 'Test Pizza',
+        description: 'test Pizza description',
+        imgSrc: 'testImg',
+        toppingIds: ['19651dda4a0af8315d840412', 'e9e565e9a57cf33fb9b8ceed'],
+        ...data,
+      },
+    ],
+    totalCount: 5,
+    hasNextPage: true,
+    cursor: '3a653aabc755306f12660be0',
+  };
+};
 const createMockPizzaDocument = (data?: Partial<PizzaDocument>): PizzaDocument => {
   return {
     _id: new ObjectId(),
     name: 'Test Pizza',
-    description: 'Test pizza description',
+    description: 'test Pizza description',
+    imgSrc: 'testImg',
     toppingIds: ['19651dda4a0af8315d840412', 'e9e565e9a57cf33fb9b8ceed'],
-    imgSrc:
-      'https://images.unsplash.com/photo-1627626775846-122b778965ae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80',
     ...data,
   };
 };
 
-export { createMockPizza, createMockPizzaDocument };
+export { createMockPizza, createMockPizzaDocument, createMockPizzaResponse };
--- packages/server/test/providers/pizza.provider.test.ts
+++ packages/server/test/providers/pizza.provider.test.ts
@@ -1,67 +1,63 @@
 import { Collection } from 'mongodb';
 import { reveal, stub } from 'jest-auto-stub';
+import { ToppingProvider } from '../../src/application/providers/toppings/topping.provider';
 import { PizzaProvider } from '../../src/application/providers/pizza/pizza.provider';
-import { mockSortToArray } from '../helpers/mongo.helper';
-import { createMockPizzaDocument } from '../helpers/pizza.helper';
+import { CursorProvider } from '../../src/application/providers/pizza/cursor.provider';
+import { mockSortToArray, mockToArray } from '../helpers/mongo.helper';
+import { createMockPizzaDocument, createMockPizzaResponse } from '../helpers/pizza.helper';
+import { createMockToppingDocument } from '../helpers/topping.helper';
 import { PizzaDocument, toPizzaObject } from '../../src/entities/pizza';
-import { ToppingProvider } from 'src/application/providers/toppings/topping.provider';
-
+import { ToppingDocument } from '../../src/entities/topping';
 const stubPizzaCollection = stub<Collection<PizzaDocument>>();
-const stubToppingProvider = stub<ToppingProvider>();
-
-const pizzaProvider = new PizzaProvider(stubPizzaCollection, stubToppingProvider);
+const stubToppingCollection = stub<Collection<ToppingDocument>>();
+const toppingProvider = new ToppingProvider(stubToppingCollection);
+const cursorProvider = new CursorProvider(stubPizzaCollection);
+const pizzaProvider = new PizzaProvider(stubPizzaCollection, toppingProvider, cursorProvider);
 
 beforeEach(jest.clearAllMocks);
 
 describe('pizzaProvider', (): void => {
   const mockPizzaDocument = createMockPizzaDocument();
   const mockPizza = toPizzaObject(mockPizzaDocument);
-
+  const mockResponse = createMockPizzaResponse();
   describe('getPizzas', (): void => {
     beforeEach(() => {
-      reveal(stubPizzaCollection).find.mockImplementation(mockSortToArray([mockPizzaDocument]));
+      reveal(stubPizzaCollection).find.mockImplementation(mockToArray([mockPizzaDocument]));
+      jest.spyOn(cursorProvider, 'getCursorResult').mockResolvedValue(mockResponse);
     });
+
     test('should call find once', async () => {
-      await pizzaProvider.getPizzas();
+      await cursorProvider.getCursorIndex('3a653aabc755306f12660be0');
       expect(stubPizzaCollection.find).toHaveBeenCalledTimes(1);
     });
 
-    test('should get all pizzas', async () => {
+    test('should get pizzaResponse', async () => {
       const result = await pizzaProvider.getPizzas();
-      expect(result).toEqual([mockPizza]);
+      expect(result).toEqual(mockResponse);
     });
   });
 
-  describe('createTopping', (): void => {
-    const validPizza = createMockPizzaDocument({
-      name: 'test pizza',
-      description: 'test pizza description',
-      toppingIds: ['19651dda4a0af8315d840412', 'e9e565e9a57cf33fb9b8ceed'],
-      imgSrc: 'test picture src',
-    });
+  describe('createPizza', (): void => {
+    const testPizzaData = {
+      name: 'create pizza',
+      description: 'create description',
+      imgSrc: 'create image',
+      toppingIds: ['19651dda4a0af8315d840412'],
+    };
 
+    const validPizza = createMockPizzaDocument(testPizzaData);
+    const validTopping = createMockToppingDocument({ name: 'test topping', priceCents: 300 });
     beforeEach(() => {
       reveal(stubPizzaCollection).findOneAndUpdate.mockImplementation(() => ({ value: validPizza }));
+      reveal(stubToppingCollection).find.mockImplementation(mockSortToArray([validTopping]));
     });
-    test('should call findOneAndUpdate once', async () => {
-      await pizzaProvider.createPizza({
-        name: validPizza.name,
-        description: validPizza.description,
-        toppingIds: validPizza.toppingIds,
-        imgSrc: validPizza.imgSrc,
-      });
 
+    test('should call findOneAndUpdate once', async () => {
+      await pizzaProvider.createPizza(testPizzaData);
       expect(stubPizzaCollection.findOneAndUpdate).toHaveBeenCalledTimes(1);
     });
-
     test('should return a pizza when passed valid input', async () => {
-      const result = await pizzaProvider.createPizza({
-        name: validPizza.name,
-        description: validPizza.description,
-        toppingIds: validPizza.toppingIds,
-        imgSrc: validPizza.imgSrc,
-      });
-
+      const result = await pizzaProvider.createPizza(testPizzaData);
       expect(result).toEqual(toPizzaObject(validPizza));
     });
   });
@@ -72,30 +68,26 @@ describe('pizzaProvider', (): void => {
     });
     test('should call findOneAndDelete once', async () => {
       await pizzaProvider.deletePizza(mockPizza.id);
-
       expect(stubPizzaCollection.findOneAndDelete).toHaveBeenCalledTimes(1);
     });
-
     test('should throw an error if findOneAndDelete returns null for value', async () => {
       reveal(stubPizzaCollection).findOneAndDelete.mockImplementation(() => ({ value: null }));
-
       await expect(pizzaProvider.deletePizza(mockPizza.id)).rejects.toThrow(new Error('Could not delete the pizza'));
     });
-
     test('should return an id', async () => {
       const result = await pizzaProvider.deletePizza(mockPizza.id);
-
       expect(result).toEqual(mockPizza.id);
     });
   });
 
   describe('updatePizza', (): void => {
-    const validPizza = createMockPizzaDocument({
+    const testPizzaData = {
       name: 'test pizza',
-      description: 'test pizza description',
-      toppingIds: ['19651dda4a0af8315d840412', 'e9e565e9a57cf33fb9b8ceed'],
-      imgSrc: 'test picture src',
-    });
+      description: 'test description',
+      imgSrc: 'test image',
+      toppingIds: ['19651dda4a0af8315d840412'],
+    };
+    const validPizza = createMockPizzaDocument(testPizzaData);
 
     beforeEach(() => {
       reveal(stubPizzaCollection).findOneAndUpdate.mockImplementation(() => ({ value: validPizza }));
@@ -104,22 +96,16 @@ describe('pizzaProvider', (): void => {
     test('should call findOneAndUpdate once', async () => {
       await pizzaProvider.updatePizza({
         id: validPizza.id,
-        name: validPizza.name,
-        description: validPizza.description,
-        toppingIds: validPizza.toppingIds,
-        imgSrc: validPizza.imgSrc,
+        ...validPizza,
       });
 
       expect(stubPizzaCollection.findOneAndUpdate).toHaveBeenCalledTimes(1);
     });
 
-    test('should return a pizza', async () => {
+    test('should return a topping', async () => {
       const result = await pizzaProvider.updatePizza({
         id: validPizza.id,
-        name: validPizza.name,
-        description: validPizza.description,
-        toppingIds: validPizza.toppingIds,
-        imgSrc: validPizza.imgSrc,
+        ...validPizza,
       });
 
       expect(result).toEqual(toPizzaObject(validPizza));
